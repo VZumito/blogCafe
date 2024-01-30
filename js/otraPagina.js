0b79295d8c61e9174e633477f0451965d9230c11
VZumito <145598892+VZumito@users.noreply.github.com>
2024-01-30 19:19:47 +0000

    Add files via upload

---
+++ js/otraPagina.js
@@ -0,0 +1,9 @@
+function otraPagina(){
+    var confirmar = confirm('Estas seguro que deseas salir?');
+    if (confirmar){
+      
+      window.location.href = "https://www.google.com"
+    
+    }else{
+    }
+    }
---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+//Seleccionar elementos
+
+//queryselector
+
+const formulario = document.querySelector(".formulario");
+console.log(formulario);
+
+// const enlaces = document.querySelectorAll('.navegacion a');
+// console.log(enlaces);
+// for (let i = 0; i < enlaces.length; i++){
+//     enlaces[i].classList.add("rojo");
+//     enlaces[i].style.color = "red";
+// }
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+const nombre = document.querySelector('#nombre')
+const email = document.querySelector('#email')
+const mensaje = document.querySelector('#mensaje')
+
+
+
+// textoHeading.textContent = "Contacto"
+
+
+// const enlaces = document.querySelectorAll('.navegacion a');
+
+// enlaces[0].textContent = "Login";
+// enlaces[3].textContent = "Login";
+
+// //enlaces[0].href="http://www.google.com";
+// enlaces[0].classList.add("nueva-clase");
+// enlaces[0].classList.remove("navegacion__enlace");
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     console.log("Documento cargado")
+// })
+// window.onscroll = function(e){
+//     console.log("Estoy haciendo scroll")
+// }
+
+
